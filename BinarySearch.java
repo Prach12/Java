4616f9975f55345c8d44a94098461a66167dc4e1
Your Name <your@email.com>
2025-05-28 13:19:31 +0530

    git add . git commit -m "Your message" git push

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,5 @@
 public class BinarySearch {
-    // Method to perform binary search
+    
     public static int binarySearch(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
@@ -7,24 +7,24 @@ public class BinarySearch {
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
-            // Check if the target is present at mid
+           
             if (arr[mid] == target) {
-                return mid; // Target found
+                return mid;
             }
-            // If target greater, ignore left half
+           
             else if (arr[mid] < target) {
                 left = mid + 1;
             }
-            // If target is smaller, ignore right half
+          
             else {
                 right = mid - 1;
             }
         }
 
-        return -1; // Target not found
+        return -1; 
     }
 
-    // Main method to test the search
+    
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
         int target = 10;
